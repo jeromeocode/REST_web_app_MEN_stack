b272ba1b500823e745b3998550f4fbb3f2704dfb
Jerome VIllapando <jvillapando@ne-rv.com>
2020-04-04 22:21:01 -0400

    mongodb interfacing, users.js get function

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.find({}, {}, function(e,docs){
+    res.json(docs); //Basically a res.send(JSON.stringify(docs));
+  });
+  //Generally you want to limit how much data gets spewed out at one time with a response
 });
 
 module.exports = router;
